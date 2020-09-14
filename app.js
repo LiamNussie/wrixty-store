cfc5618c48860c46366110ba05dce4c93834b01c
LiamNussie <65913823+LiamNussie@users.noreply.github.com>
2020-09-14 23:40:47 +0100

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('#menu');
+const navLinks = document.querySelector('ul');
+const searchBox = document.querySelector('#search');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('change');
+    if(menu.classList.contains('change')) {
+        navLinks.style.display = 'block';
+        searchBox.style.display = 'none';
+        setTimeout(() => {
+            navLinks.style.animation = 'dropDown 0.5s linear 1'
+            navLinks.style.transform = 'translate(-20px,0)'
+        }, 300) 
+    } else {
+            navLinks.style.display = 'none';
+            searchBox.style.display = 'inline-block';
+    }
+});
