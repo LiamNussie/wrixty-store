2ae7d3905747dde4d2429d659875fa273cb0c568
LiamNussie <65913823+LiamNussie@users.noreply.github.com>
2020-09-15 16:48:05 +0100

    edit

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const searchBox = document.querySelector('#search');
 menu.addEventListener('click', () => {
     menu.classList.toggle('change');
     if(menu.classList.contains('change')) {
-        navLinks.style.display = 'block';
-        searchBox.style.display = 'none';
+        // navLinks.style.display = 'block';
         setTimeout(() => {
             navLinks.style.animation = 'dropDown 0.5s linear 1'
             navLinks.style.transform = 'translate(-20px,0)'
         }, 300) 
     } else {
             navLinks.style.display = 'none';
-            searchBox.style.display = 'inline-block';
     }
 });
